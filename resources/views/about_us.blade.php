20e8f01da0bb158b0604cb13f048335e4561190b
Saadq20 <64100828+Saadq20@users.noreply.github.com>
2022-11-19 20:56:50 +0300

    Update about_us.blade.php

--- resources/views/about_us.blade.php
+++ resources/views/about_us.blade.php
@@ -64,76 +64,7 @@
                                     <img class="img" src="../assets/img/waleed.jpg">
                                 </a>
                             </div>
-                            <div class="card-body">
-                                <h4 class="card-title">Waleed ALqahtani</h4>
-                                <h6 class="category text-muted">IS student</h6>
-                                <p class="card-description">
-                                    I'm student in information system major at king khalid unviersity.
-                                </p>
-                            </div>
-                            <div class="card-footer justify-content-center">
-                                <a href="#pablo" class="btn btn-just-icon btn-link btn-twitter">
-                                    <i class="fa fa-twitter"></i>
-                                </a>
-                                <a href="#pablo" class="btn btn-just-icon btn-link btn-dribbble">
-                                    <i class="fa fa-dribbble"></i>
-                                </a>
-                                <a href="#pablo" class="btn btn-just-icon btn-link btn-linkedin">
-                                    <i class="fa fa-linkedin"></i>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-3">
-                        <div class="card card-profile card-plain">
-                            <div class="card-avatar">
-                                <a href="#pablo">
-                                    <img class="img" src="../assets/img/man.jpg">
-                                </a>
-                            </div>
-                            <div class="card-body">
-                                <h4 class="card-title">Mohammed Alshahrani</h4>
-                                <h6 class="category text-muted">IS student</h6>
-                                <p class="card-description">
-                                    I'm student in information system major at king khalid unviersity.
-                                </p>
-                            </div>
-                            <div class="card-footer justify-content-center">
-                                <a href="#pablo" class="btn btn-just-icon btn-link btn-facebook"><i
-                                        class="fa fa-facebook-square"></i></a>
-                                <a href="#pablo" class="btn btn-just-icon btn-link btn-dribbble"><i
-                                        class="fa fa-dribbble"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-3">
-                        <div class="card card-profile card-plain">
-                            <div class="card-avatar">
-                                <a href="#pablo">
-                                    <img class="img" src="../assets/img/man.jpg">
-                                </a>
-                            </div>
-                            <div class="card-body">
-                                <h4 class="card-title">Saad AL-asiri</h4>
-                                <h6 class="category text-muted">IS student</h6>
-                                <p class="card-description">
-                                    I'm student in information system major at king khalid unviersity.
-                                </p>
-                            </div>
-                            <div class="card-footer justify-content-center">
-                                <a href="#pablo" class="btn btn-just-icon btn-link btn-google"><i
-                                        class="fa fa-google"></i></a>
-                                <a href="#pablo" class="btn btn-just-icon btn-link btn-twitter"><i
-                                        class="fa fa-twitter"></i></a>
-                                <a href="#pablo" class="btn btn-just-icon btn-link btn-dribbble"><i
-                                        class="fa fa-dribbble"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            
-            </div>
+                           
             
             <div class="about-contact">
                 <div class="row">
